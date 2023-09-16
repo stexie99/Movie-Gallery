a595a9331d2ee3f94169c5c9457d6614dbb7684a
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-16 14:27:50 -0400

    format movie seeder

--- models/movieSeedData.js
+++ models/movieSeedData.js
@@ -1,74 +1,74 @@
 module.exports = [
-    {
-        title: "Frozen",
-        year: "2013",
-        director: "Chris Buck",
-        genre: "kid film"
-    },
-    {
-        title: "Lion King",
-        year: "1994",
-        director: "George Scribner",
-        genre: "musical drama"
-    },
-    {
-        title: "Zootopia",
-        year: "2016",
-        director: "Byron howard",
-        genra: "Action comedy"
-    },
-    {
-      title: 'Pretty Woman',
-      year: '1994',
-      director: 'Garry Marshall',
-      genre: 'Romance'
-    },
-    {
-        title: 'True Lies',
-        year: '1994',
-        director: 'James Cameron',
-        genre: 'Action'
-    },
-    {
-        title: 'The Sixth',
-        year: '1999',
-        director: 'M. Night Shyamalan',
-        genre: 'Thriller'
-    },
-    {
-        title: "Spider-Man: Across the Spider-Verse",
-        year: "2023",
-        director: "Joaquim Dos Santos",
-        genre: "action"
-    },
-    {
-        title: "Whiplash",
-        year: "2014",
-        director: "Damien Chazelle",
-        genre: "Drama"
-    },
-    {
-        title: "Interstellar",
-        year: "2014",
-        director: "Christopher Nolan",
-        genre: "Science Fiction"
-    },
-    {
-        title: "The Shawshank Redemption",
-        year: 1994,
-        director: "Frank Darabont",
-        genre: "Drama"
-    },
-    {
-        title: "The Godfather",
-        year: 1972,
-        director: "Francis Ford Coppola",
-        genre: "Crime"
-    },
-    {
-        title: "Star Wars: Episode IV - A New Hope",
-        year: 1977,
-        director: "George Lucas",
-        genre: "Action"
-    }
+  {
+    title: "Frozen",
+    year: "2013",
+    director: "Chris Buck",
+    genre: "kid film",
+  },
+  {
+    title: "Lion King",
+    year: "1994",
+    director: "George Scribner",
+    genre: "musical drama",
+  },
+  {
+    title: "Zootopia",
+    year: "2016",
+    director: "Byron howard",
+    genra: "Action comedy",
+  },
+  {
+    title: "Pretty Woman",
+    year: "1994",
+    director: "Garry Marshall",
+    genre: "Romance",
+  },
+  {
+    title: "True Lies",
+    year: "1994",
+    director: "James Cameron",
+    genre: "Action",
+  },
+  {
+    title: "The Sixth",
+    year: "1999",
+    director: "M. Night Shyamalan",
+    genre: "Thriller",
+  },
+  {
+    title: "Spider-Man: Across the Spider-Verse",
+    year: "2023",
+    director: "Joaquim Dos Santos",
+    genre: "action",
+  },
+  {
+    title: "Whiplash",
+    year: "2014",
+    director: "Damien Chazelle",
+    genre: "Drama",
+  },
+  {
+    title: "Interstellar",
+    year: "2014",
+    director: "Christopher Nolan",
+    genre: "Science Fiction",
+  },
+  {
+    title: "The Shawshank Redemption",
+    year: "1994",
+    director: "Frank Darabont",
+    genre: "Drama",
+  },
+  {
+    title: "The Godfather",
+    year: "1972",
+    director: "Francis Ford Coppola",
+    genre: "Crime",
+  },
+  {
+    title: "Star Wars: Episode IV - A New Hope",
+    year: "1977",
+    director: "George Lucas",
+    genre: "Action",
+  },
 ]
