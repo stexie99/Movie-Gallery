e75e2669ac44f82678dfaf7b101a5c23bdc560e3
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-16 13:55:33 -0400

    make models

---
+++ models/movieSeedData.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        title: "The Shawshank Redemption",
+        year: 1994,
+        director: "Frank Darabont",
+        genre: "Drama"
+    },
+    {
+        title: "The Godfather",
+        year: 1972,
+        director: "Francis Ford Coppola",
+        genre: "Crime"
+    },
+    {
+        title: "Star Wars: Episode IV - A New Hope",
+        year: 1977,
+        director: "George Lucas",
+        genre: "Action"
+    },
+]
