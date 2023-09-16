d617c0bca0a1800cdc1ab81dc2aa20be23211df6
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-09-16 14:12:50 -0400

    update seeder info

---
+++ models/movieSeedData.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+      title: 'Pretty Woman',
+      year: '1994-09-22',
+      director: 'Garry Marshall',
+      genre: 'Romance'
+    },
+    {
+        title: 'True Lies',
+        year: '1994',
+        director: 'James Cameron',
+        genre: 'Action'
+    },
+    {
+        title: 'The Sixth',
+        year: '1999',
+        director: 'M. Night Shyamalan',
+        genre: 'Thriller'
+    }
+]
