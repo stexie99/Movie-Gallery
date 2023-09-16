99eac473e2381c3bad3c42196bfbd7738dab86ff
mandeepkaur86 <mandeepkaurdia@gmail.com>
2023-09-16 11:17:50 -0700

    updataseeder

---
+++ models/movieSeedData.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+        title: "Frozen",
+        year: "2013",
+        director: "Chris Buck",
+        genre: "kid film"
+    },{
+        title: "Lion King",
+        year: "1994",
+        director: "George Scribner",
+        genre: "musical drama"
+    },{
+        title: "Zootopia",
+        year: "2016",
+        director: "Byron howard",
+        genra: "Action comedy"
+    }
+
+]
