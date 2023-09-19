33289207ad69cfdaa185bfde4c1635041efbb916
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-19 12:13:14 -0400

    add favicon

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,7 @@ function Default(html) {
     <html>
       <head>
         <title>Default</title>
+        <link rel="icon" type="image/png" href="/images/favicon.png" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"></link>
         <link rel="stylesheet" href="/css/style.css" />
       </head>
