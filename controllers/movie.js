f54b2fe5b7fa3297e3ad68be94536f3b0d1f4527
Steven Xie <xies1999@hotmail.com>
2023-09-20 01:14:23 -0400

    finished building basic structure of controllers

--- controllers/movie.js
+++ controllers/movie.js
@@ -12,9 +12,55 @@ router.get('/', async(req, res)=>{
     res.send(movies)
 })
 
+router.get('/:name', async (req, res) => {
+    try {
+      const { name } = req.params
+      // Create a regex pattern that matches the provided 'name' with case and space insensitivity
+      const regexPattern = new RegExp(`^${name.replace(/\s+/g, '\\s*')}$`, 'i')
+      
+      const movie = await Movie.findOne({ title: { $regex: regexPattern } })
+      //creates search using $regex pattern
+      if (!movie) {
+        return res.status(404).json({ error: 'Movie not found' })
+        }  
+      res.status(200).json(movie)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+})
+
 router.post('/', async(req, res) =>{
-    console.log(req.body)
-    res.send("hello")
+  await Movie.create(req.body)
+  console.log(req.body)
+  res.send("hello")
+})
+
+router.put('/:name', async(req,res)=>{
+  const { name } = req.params
+  try{
+    const updatedMovie = await Movie.findOneAndUpdate({ title: name }, req.body)
+    if(!updatedMovie){
+      return res.status(404).json({ error:'movie not found' })
+    }
+    res.status(200).json(updatedMovie)
+  }catch(err){
+    res.status(500).json(err)
+  }
+  console.log('hello')
+})
+
+router.delete('/:name', async (req, res) => {
+  const { name } = req.params
+  try {
+    const deletedMovie = await Movie.findOneAndDelete({ title: name })
+    if (!deletedMovie) {
+      return res.status(404).json({ error: 'Movie not found' })
+    }
+    res.status(200).json({ message: 'Movie deleted successfully' })
+  } catch (err) {
+    console.error('Error:', err)
+    res.status(500).json(err)
+  }
 })
 
 module.exports = router
