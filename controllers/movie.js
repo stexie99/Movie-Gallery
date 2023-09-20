8c096e39cf5de1518fff6b7d4a7acb8710f722d5
Steven Xie <xies1999@hotmail.com>
2023-09-20 18:32:25 -0400

    changed index route to previous version

--- controllers/movie.js
+++ controllers/movie.js
@@ -12,21 +12,33 @@ router.get('/', async(req, res)=>{
     res.send(movies)
 })
 
-router.get('/:name', async (req, res) => {
-    try {
-      const { name } = req.params
-      // Create a regex pattern that matches the provided 'name' with case and space insensitivity
-      const regexPattern = new RegExp(`^${name.replace(/\s+/g, '\\s*')}$`, 'i')
+// router.get('/:name', async (req, res) => {
+//     try {
+//       const { name } = req.params
+//       // Create a regex pattern that matches the provided 'name' with case and space insensitivity
+//       const regexPattern = new RegExp(`^${name.replace(/\s+/g, '\\s*')}$`, 'i')
       
-      const movie = await Movie.findOne({ title: { $regex: regexPattern } })
-      //creates search using $regex pattern
-      if (!movie) {
-        return res.status(404).json({ error: 'Movie not found' })
-        }  
-      res.status(200).json(movie)
-    } catch (err) {
-      res.status(500).json(err)
+//       const movie = await Movie.findOne({ title: { $regex: regexPattern } })
+//       //creates search using $regex pattern
+//       if (!movie) {
+//         return res.status(404).json({ error: 'Movie not found' })
+//         }  
+//       res.status(200).json(movie)
+//     } catch (err) {
+//       res.status(500).json(err)
+//     }
+// })
+router.get('/:name', async (req, res) => {
+  try {
+    const { name } = req.params
+    const movie = await Movie.findOne({ title: name })
+    if (!movie) {
+      return res.status(404).json({ error: 'Movie not found' })
     }
+    res.status(200).json(movie)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 router.post('/', async(req, res) =>{
