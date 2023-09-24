4b959ac144ae0f923ac93ba26de7472882914066
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-24 17:56:39 -0400

    fix error404 image link

--- views/error404.jsx
+++ views/error404.jsx
@@ -5,7 +5,7 @@ function Error404() {
     return (
         <Default>
              <h1>404: PAGE NOT FOUND</h1>
-              <img width='50%'src='./images/404.jpg' alt='404error' />
+              <img width='50%'src='../images/404.jpg' alt='404error' />
               <p>Oops, sorry, we can't find this page!</p>
           
         </Default>
