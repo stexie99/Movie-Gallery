f82bc95e33ddd24390a32c4f462fa725dcb992c4
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-25 01:32:03 -0400

    add subtle animation to image links in the gallery

--- views/error404.jsx
+++ views/error404.jsx
@@ -2,14 +2,15 @@ const React = require("react")
 const Default = require("./default")
 
 function Error404() {
-    return (
-        <Default>
-             <h1>404: PAGE NOT FOUND</h1>
-              <img width='50%'src='../images/404.jpg' alt='404error' />
-              <p>Oops, sorry, we can't find this page!</p>
-          
-        </Default>
-    )
+  return (
+    <Default>
+      <body className="error404">
+        <h1>404: PAGE NOT FOUND</h1>
+        <img width="50%" src="../images/404.jpg" alt="404error" style={{ opacity: 0.9 }}/>
+        <p>Oops, sorry, we can't find this page!</p>
+      </body>
+    </Default>
+  )
 }
 
 module.exports = Error404
